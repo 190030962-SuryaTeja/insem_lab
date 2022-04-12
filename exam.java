96f5da13c282697321d350b01cba53f12b4e38c2
190030962-SuryaTeja <98305014+190030962-SuryaTeja@users.noreply.github.com>
2022-04-12 16:02:03 +0530

    commit for lab insem

---
+++ exam.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class exam
+{
+	public static void main(String args[])
+	{
+		int a = 10;
+		int b = -5;
+		int sum = a + b;
+		System.out.println("Sum of the given numbers is = ",sum);
+	}
+} 
